67adcdd03fa9b680194f9ab99239296c1085c10d
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2016-12-20 18:20:56 -0600

    Add functionality to add new To Do List items.

--- practice/ToDoList/js/todo.js
+++ practice/ToDoList/js/todo.js
@@ -1,12 +1,28 @@
+/*
+Why Use On()?
+- click() only adds listeners for existing elements
+- on() will add listeners for all potential future elements
+*/
+
 // Check off specific To Dos by clicking
-$("li").click(function() {
+// Add listener to parent so that any 'li' element will be affected.
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 
 // Click on X to delete a To Do
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
   $(this).parent().fadeOut(500, function() {
     $(this).remove();
   })
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    // <enter> pressed
+    var toDoText = $(this).val();
+    $("ul").append("<li><span>X</span> " + toDoText + "</li>")
+    $(this).val("");
+  }
 })
