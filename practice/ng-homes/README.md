da545efb21cf09e66e90694e47e5953067004868
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2017-03-23 18:59:46 -0500

    Added CRUD functionality.

---
+++ practice/ng-homes/README.md
@@ -0,0 +1,4 @@
+# Udemy Web Developer Course
+
+This code was derived from the following course:
+[Ultimate Web Developer](https://www.udemy.com/web-developer-course/learn/v4/overview)
--- practice/ng-homes/scripts/homesController.js
+++ practice/ng-homes/scripts/homesController.js
@@ -1,16 +1,43 @@
 angular
-.module('ngHomes')
-.controller('homesController', function($scope, homesFactory) {
+	.module('ngHomes')
+	.controller('homesController', function($scope, homesFactory) {
 
-  $scope.homes;
+		$scope.homes;
 
-  homesFactory.getHomes().then(function successCallback(response) {
-    $scope.homes = response.data;
-  }, function errorCallback(response) {
-    console.log(response.statusText);
-  });
+		$scope.priceInfo = {
+			min: 0,
+			max: 1000000
+		}
 
-  $scope.sayHello = function() {
-    console.log("Hello");
-  }
-});
+		$scope.newListing = {};
+
+		$scope.addHome = function(newListing) {
+			newListing.image = 'default-Home';
+			$scope.homes.push(newListing);
+			$scope.newListing = {};
+		}
+
+		$scope.editHome = function(Home) {
+			$scope.editListing = true;
+			$scope.existingListing = Home;
+		}
+
+		$scope.saveHomeEdit = function() {
+			$scope.existingListing = {};
+			$scope.editListing = false;
+		}
+
+		$scope.deleteHome = function(listing) {
+			var index = $scope.homes.indexOf(listing);
+			$scope.homes.splice(index, 1);
+			$scope.existingListing = {};
+			$scope.editListing = false;
+		}
+
+		homesFactory.getHomes().success(function(data) {
+			$scope.homes = data;
+		}).error(function(error) {
+			console.log(error);
+		});
+
+	});
