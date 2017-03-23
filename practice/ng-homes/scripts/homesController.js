f51367610aa863a7cf817014676c0ec461072572
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2017-03-23 16:52:03 -0500

    Created an HTTP service to simulate calling a backend server. Moved sample data to a separate file.

--- practice/ng-homes/scripts/homesController.js
+++ practice/ng-homes/scripts/homesController.js
@@ -1,5 +1,12 @@
 angular
 .module('ngHomes')
 .controller('homesController', function($scope, homesFactory) {
-  $scope.homes = homesFactory.getHomes();
+
+  $scope.homes;
+
+  homesFactory.getHomes().then(function successCallback(response) {
+    $scope.homes = response.data;
+  }, function errorCallback(response) {
+    console.log(response.statusText);
+  });
 });
